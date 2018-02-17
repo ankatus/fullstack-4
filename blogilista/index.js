acfde1a5a24840e1b4eb3c862aa1ab90e232c8ab
ankatus <antti.j.kataja@gmail.com>
2018-02-17 14:10:33 +0200

    asdf

---
+++ blogilista/index.js
@@ -0,0 +1,21 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+require('dotenv').config()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+mongoose.Promise = global.Promise
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
